ebb865215ff57a5072e654c1e1038f47b611511e
janhajk <janhajk@gmail.com>
2014-11-25 11:35:45 +0000

    replace string with DOM-procedures

--- bin/js/button.js
+++ bin/js/button.js
@@ -79,7 +79,7 @@ oButton.prototype.createSvg = function(type) {
     svg.setAttributeNS(null, 'version', '1.1');
 
     var innerSVG = window.janframe.icons.svg[this.type];
-    innerSVG.replace(/(\r\n|\n|\r)/gm, '');
+    innerSVG.replace(/(\\r\\n|\\n|\\r)/gm, '');
     var dXML = new DOMParser();
     dXML.async = false;
     sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\'>' + innerSVG + '</svg>';

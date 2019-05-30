cf2005e87aef7071661811583c6b4ea56967e2f1
janhajk <janhajk@gmail.com>
2019-05-30 12:44:25 +0000

    bug

--- public/block.js
+++ public/block.js
@@ -4,8 +4,7 @@
 
 
       var Block = function _Block(_id) {
-
-            this._id = _id;
+            this.id = (typeof _id === 'string') ? _id : '';
             this.creator = {};
             this.owner = {};
             this.timestamp = 0;
@@ -68,8 +67,8 @@
             this.create = create;
 
             // new Block
-            if (_id === undefined) {
-                  create({}, function(e) {
+            if (typeof _id === 'object') {
+                  create(_id, function(e) {
 
                   });
             }
@@ -117,8 +116,8 @@
                         for (let i in block) {
                               self[i] = block[i];
                         }
-                        async.eachOf(block.children, function(child, key, callback) {
-                                    block.children[key] = new _Block(child);
+                        async.eachOf(block.children, function(childId, key, callback) {
+                                    block.children[key] = new _Block(childId);
                                     block.children[key].load(function() {
                                           callback(); // report child loaded
                                     });
@@ -250,21 +249,6 @@
                         div.appendChild(divBody);
                         self.row.col.appendChild(div);
 
-
-
-                        // Inside head
-                        //   <div class="ibox-tools ">
-                        //       <a class="dropdown-toggle font-18 " data-toggle="dropdown "><i class="ti-ticket"></i></a>
-                        //       <div class="dropdown-menu dropdown-menu-right ">
-                        //           <a class="dropdown-item "><i class="ti-pencil mr-2 "></i>Create</a>
-                        //           <a class="dropdown-item "><i class="ti-pencil-alt mr-2 "></i>Edit</a>
-                        //           <a class="dropdown-item "><i class="ti-close mr-2 "></i>Remove</a>
-                        //       </div>
-                        //   </div>
-
-                        // Inside divSlimScroll
-                        // <div class="slimScrollBar " style="background: rgb(113, 128, 143) none repeat scroll 0% 0%; width: 4px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 437.426px; "></div>
-                        // <div class="slimScrollRail " style="width: 4px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51) none repeat scroll 0% 0%; opacity: 0.9; z-index: 90; right: 1px; "></div>
 
                   };
 

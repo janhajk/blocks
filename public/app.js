e5d80bdabc2b0453d97a8c4fecd263dcb17c15ad
janhajk <janhajk@gmail.com>
2019-06-01 09:12:40 +0000

    bug

--- public/app.js
+++ public/app.js
@@ -4,8 +4,8 @@
 (function() {
 
     var currentBlockId = '';
-    window.currentBlockId = currentBlockId;
     currentBlockId = '5cefd7185aa420404caf7628';
+    window.currentBlockId = currentBlockId;
     var b = new Block(currentBlockId, document.getElementById('content'));
     b.load(function() {
         console.log('success, fallowing your block:');

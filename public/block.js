f3a6c5e282c40e12b25f8d6af0d5db8b8a0f9af2
janhajk <janhajk@gmail.com>
2019-06-11 13:20:56 +0000

    bug

--- public/block.js
+++ public/block.js
@@ -126,7 +126,7 @@
                         self.render();
                         // Go trough all children and load them
                         // Limit 1/serial loading is important, or the order can get messed up, because on might load faster than the other
-                        async.eachOfLimit(self.data.children, 1, function(childId, key, callback) {
+                        async.eachOf(self.data.children, function(childId, key, callback) {
                                     self.data.children[key] = new _Block(childId, self.dom.row);
                                     self.data.children[key].level = self.level + 1;
                                     self.data.children[key].load(function() {

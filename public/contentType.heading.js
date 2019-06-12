164cc5e751ccda7807b170fb577ff08401e639f3
janhajk <janhajk@gmail.com>
2019-06-12 16:13:17 +0000

    bug

--- public/contentType.heading.js
+++ public/contentType.heading.js
@@ -5,7 +5,7 @@
 
       Block.fn.contentType.heading = {
             content: function(block) {
-                  return '<h' + (block.level + 1) + '>' + block.data.content + '</h' + (block.level + 1) + '>';
+                  return '<h' + (block.level + 2) + '>' + block.data.content + '</h' + (block.level + 2) + '>';
             }
 
       };
